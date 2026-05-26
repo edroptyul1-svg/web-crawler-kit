88ea36cc590ed97c3b327482d907280608ee01fe
edroptyul1-svg <edroptyul1@gmail.com>
2026-05-26 13:02:39 +0700

    add main application

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Web crawler toolkit with configurable depth and domain restrictions"""
+
+class App:
+    def __init__(self):
+        self.config = {}
+    
+    def run(self):
+        print("Running web-crawler-kit...")
+        return True
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
